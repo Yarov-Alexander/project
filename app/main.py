c147292d693e08fc9cdc67ce42885ff94175542b
Alex <aaaaesvd@gmail.com>
2026-03-24 14:45:59 +0300

    included routers

--- app/main.py
+++ app/main.py
@@ -1,10 +1,18 @@
 from fastapi import FastAPI, HTTPException, status
 from .modules.categories.routers import router as categories_router
+from app.modules.users.routers import router as users_router
+from app.modules.CartItem.routers import router as cart_router
+from app.modules.reviews.routers import router as reviews_router
+from app.modules.products.routers import router as products_router
 
 app = FastAPI()
 app.include_router(categories_router)
-@app.get("/lop/{lop}", status_code=200)
+app.include_router(users_router)
+app.include_router(cart_router)
+app.include_router(reviews_router)
+app.include_router(products_router)
+@app.get("/", status_code=200)
 def check(lop: str):
     if not lop.isalpha():
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Invalid lop ")
-    return {"Message": f"Hello {lop}!"}
+    return {"Message": f"Hello!"}
